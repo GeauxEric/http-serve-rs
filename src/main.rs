22e477f5eabc9281c939cade903db4d3a1a9764d
Yun Ding <yunding.eric@gmail.com>
2023-11-23 01:39:19 -0800

    guess http content type and return file content

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,73 @@
+use std::path::{Path as FsPath, PathBuf};
+
 use anyhow::anyhow;
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Response};
-use axum::{routing::get, Router};
+use axum::{http, routing::get, Router};
+
+fn fs_path_to_url_path(p: &FsPath) -> String {
+    let s = p.to_str().unwrap();
+    return if let Some(p) = s.strip_prefix('.') {
+        p.to_string()
+    } else {
+        s.to_string()
+    };
+}
+
+#[test]
+fn test_path_to_href() {
+    let path = FsPath::new("./");
+    assert_eq!(fs_path_to_url_path(path), "/");
+
+    let path = FsPath::new(".");
+    assert_eq!(fs_path_to_url_path(path), "");
+}
 
 async fn list_pwd() -> Result<Html<String>, AppErr> {
-    let mut dir = tokio::fs::read_dir(".")
-        .await
-        .map_err(|e| AppErr(anyhow::Error::from(e)))?;
+    return list_dir(FsPath::new(".")).await;
+}
+
+async fn get_file_or_list_dir(Path(url_path): Path<String>) -> Response {
+    let mut fs_path = PathBuf::from(".");
+    fs_path.push(url_path);
+    if fs_path.is_dir() {
+        return match list_dir(&fs_path).await {
+            Ok(html) => html.into_response(),
+            Err(e) => e.into_response(),
+        };
+    }
+    if fs_path.is_file() {
+        return match tokio::fs::read(&fs_path).await {
+            Ok(content) => {
+                let mime_type = mime_guess::from_path(&fs_path)
+                    .first()
+                    .map(|m| m.to_string())
+                    .unwrap_or("application/octet-stream".to_string());
+                (
+                    StatusCode::OK,
+                    [(http::header::CONTENT_TYPE, mime_type)],
+                    content,
+                )
+                    .into_response()
+            }
+            Err(e) => AppErr::from(e).into_response(),
+        };
+    }
+    AppErr(anyhow!("unhandled type")).into_response()
+}
+
+async fn list_dir(p: &FsPath) -> Result<Html<String>, AppErr> {
+    let mut dir = tokio::fs::read_dir(p).await?;
     let mut html_content =
         String::from("<html><head><title>Directory Listing</title></head><body><ul>");
 
-    while let Some(entry) = dir
-        .next_entry()
-        .await
-        .map_err(|e| AppErr(anyhow::Error::from(e)))?
-    {
+    while let Some(entry) = dir.next_entry().await? {
         let file_name = entry
             .file_name()
             .into_string()
-            .map_err(|_e| AppErr(anyhow!("[Invalid UTF-8]")))?;
-        let metadata = entry
-            .metadata()
-            .await
-            .map_err(|e| AppErr(anyhow::Error::from(e)))?;
+            .unwrap_or_else(|_| String::from("[Invalid UTF-8]"));
+        let metadata = entry.metadata().await?;
         let suffix = if metadata.is_dir() {
             "/"
         } else if metadata.is_symlink() {
@@ -30,28 +75,42 @@ async fn list_pwd() -> Result<Html<String>, AppErr> {
         } else {
             ""
         };
+        let mut dir = p.to_path_buf();
+        dir.push(FsPath::new(&file_name));
+        let href = fs_path_to_url_path(&dir);
         html_content.push_str(&format!(
             "<li><a href=\"{}\">{}{}</a></li>",
-            file_name, file_name, suffix
+            href, file_name, suffix
         ));
     }
 
     html_content.push_str("</ul></body></html>");
-    Ok(Html::from(html_content))
+    return Ok(Html(html_content));
 }
 
 struct AppErr(anyhow::Error);
 
 impl IntoResponse for AppErr {
     fn into_response(self) -> Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+        (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+    }
+}
+
+impl<E> From<E> for AppErr
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
     }
 }
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(list_pwd));
+    let app = Router::new()
+        .route("/", get(list_pwd))
+        .route("/*fs_path", get(get_file_or_list_dir));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
